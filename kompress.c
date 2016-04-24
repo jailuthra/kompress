cf259a44eaf4c1728a258cc6dd5ce203e25b9fbc
Jai Luthra <me@jailuthra.in>
2016-04-24 05:43:40 +0530

    main: restructure

--- kompress.c
+++ kompress.c
@@ -1,28 +1,66 @@
 #include<stdio.h>
 
+struct treenode {
+    char data;
+    int freq;
+    struct treenode *l;
+    struct treenode *r;
+    struct treenode *p;
+};
+
+int create_freq_table(FILE *fp, struct treenode *ftable);
+
 int main(int argc, char *argv[])
 {
-    FILE *fptr;
+    FILE *fp;
     if (argc == 2) {
-        if (!(fptr = fopen(argv[1], "r"))) {
+        if (!(fp = fopen(argv[1], "r"))) {
             fprintf(stderr, "Couldn't open file %s\n", argv[1]);
         }
     } else {
-        // STDIN mode
-        fptr = stdin;
+        /* STDIN mode */
+        fp = stdin;
     }
+    /* Get table of frequencies (> 0) of characters in text */
+    struct treenode ftable[128];
+    int ftsize = create_freq_table(fp, ftable);
+    int i;
+    for (i=0; i<ftsize; i++) {
+        printf("%c (%d): %d\n", ftable[i].data, ftable[i].data, ftable[i].freq);
+    }
+    rewind(fp);
+    fclose(fp);
+    return 0;
+}
+
+/** Create a frequency table from the input stream.
+ *
+ * @param[in] fp Pointer to input stream
+ * @param[out] ftable Frequency table
+ * @retval ftsize Number of elements in ftable
+ */
+int create_freq_table(FILE *fp, struct treenode *ftable)
+{
     int freq[128], i;
     char c;
-    for (i=0; i<127; i++) {
+    /* set initial frequencies to 0 */
+    for (i=0; i<128; i++) {
         freq[i] = 0;
     }
-    while ((c = fgetc(fptr)) != EOF) {
+    /* update frequencies of each ascii value from file */
+    while ((c = fgetc(fp)) != EOF) {
         freq[(int) c] += 1;
     }
+    /* update table */
+    struct treenode *node;
+    int top = 0;
     for (i=0; i<127; i++) {
         if (freq[i] != 0) {
-            printf("%c (%d): %d\n", (char) i, i, freq[i]);
+            node = &ftable[top++];
+            node->data = (char) i;
+            node->freq = freq[i];
+            node->l = node->r = NULL;
         }
     }
-    return 0;
+    return top;
 }
